2e283702c0b50599e432322681da0b9ef6636d9f
Gowtham kumar <kumargowtham.j@gmail.com>
2023-09-08 22:52:47 +0530

    basic implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
+import { useState } from 'react'
 import QRCode from 'qrcode'
 import './App.css'
+import Header from './Components/Header'
 
 function App() {
 
+  const canvas = document.getElementById('qrcode')
+  const [urlText, setUrlText] = useState("");
+
+  function generateQR() {
+    if(urlText != "") {
+      QRCode.toCanvas(canvas, urlText, (err) => {
+        if(err) console.error(err); 
+      })
+    }
+  }
+
+  function updateUrlText(e) {
+      setUrlText(e.target.value);
+  }
+
+  function downloadQR() {
+    const canvasUrl = canvas.toDataURL();
+    // Create an anchor, and set the href value to our data URL
+    const createEl = document.createElement('a');
+    createEl.href = canvasUrl;
+
+    // This is the name of our downloaded file
+    createEl.download = "qr";
+
+    // Click the download button, causing a download, and then remove it
+    createEl.click();
+    createEl.remove();
+  }
+
   return (
-    <div>
-      <h1>{import.meta.env.VITE_APP_NAME}</h1>
+    <div className='app'>
+      <Header />
+      <div className='tab'>QR Generator</div>
+      <input type="text" placeholder="Enter a Text..." id='inputBox' onChange={updateUrlText} />
+      <button onClick={generateQR}>Generate QR</button>
+      <canvas id='qrcode'></canvas>
+      <button onClick={downloadQR}>Download</button>
     </div>
   )
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <header className="header">
+            <div className="logo">QR Panda</div>
+            <div className="nav-menu">
+                <a className="nav-menu-item" href="https://github.com/gowthamk17/qrpanda" target="_blank">Source code</a>
+            </div>
+        </header>
+    );
+}
+
+export default Header
