56e66802253a5beee0d387ef3304ed2d22915e49
Gowtham kumar <kumargowtham.j@gmail.com>
2023-09-13 20:41:39 +0530

    pwa change

--- vite.config.ts
+++ vite.config.ts
@@ -8,31 +8,6 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'QR Panda',
-        short_name: 'QR Panda',
-        description: 'QR Code Generator free open source',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
     }),
   ],
   base: '/qrpanda/'
